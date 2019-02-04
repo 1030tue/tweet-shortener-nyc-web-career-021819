b6c34751969ff2a1e92c2ab4de059e6c446a7d18
Kate <katekim5809@gmail.com>
2019-02-04 23:51:55 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,14 @@
 # Write your code here.
 
-dictionary = {"hello" => "hi","to" => "2", "two" => "2", "too" => '2', "for" => "4", "four" => "4", "be" => "b", "you" ="u", "at" => "@", "and" => "&"}
+dictionary = {"hello" => "hi","to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" =>"u", "at" => "@", "and" => "&"}
+
+ def word_substituter(tweet)
+ arr = tweet.split(" ")
+ arr.map! {|w| w.downcase }
+ arr = arr.map do |w|
+ 	dictionary.fetch(w, w)
+ end
+ arr.join(" ")
+end
+
 

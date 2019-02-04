76c91e73c9c9c617a85af2cbe5b11f55bbcc05b2
Kate <katekim5809@gmail.com>
2019-02-04 22:46:44 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,6 @@
 # Write your code here.
 
-dictionary = {"hello" => 'hi',"to" => "2", two, too" become '2' 
-"for, four" become '4'
+dictionary = {"hello" => "hi","to" => "2", "two" => "2", "too" => '2', "for" => 4, four" become '4'
 'be' becomes 'b'
 'you' becomes 'u'
 "at" becomes "@" 
